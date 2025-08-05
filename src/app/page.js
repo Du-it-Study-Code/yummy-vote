4dc8c8185b7ec2793435fe8c754e1525e787e23f
yebhin <yebhin@duranno.com>
2025-08-05 13:40:35 +0900

    메인 페이지 높이값 화면 100vh 고정

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,26 @@
 // 요약: 홈 페이지 (주문하러 가기 버튼)
 
 import Header from '@/components/layouts/Header';
+import LogoHeader from '@/components/layouts/LogoHeader';
 import Footer from '@/components/layouts/Footer';
 import Container from '@/components/layouts/Container';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
-      {/* Header 영역 */}
-
-      {/* Main 영역 */}
-      <main className="flex-1">
-        <Container className="py-8">
-          <div className="flex flex-col space-y-4 border-2 p-4">
-            {/* 섹션 제목 */}
-            <button className="w-full py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-orange-500 hover:bg-orange-300 cursor-pointer">주문하기</button>
-            <span className="text-base font-light tracking-tight text-center text-black"><a>회원가입</a> / <a>로그인</a></span>
-
-          </div>
-        </Container>
+    <div className="flex flex-col min-h-screen">
+      {/* <Header /> */}
+      <LogoHeader />
+      <main className="flex-1 flex items-center justify-center px-4">
+        <div className="w-full flex flex-col space-y-4 border-2 p-4">
+          <button className="w-full py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-orange-500 hover:bg-orange-300 cursor-pointer">
+            주문하기
+          </button>
+          <span className="text-base font-light tracking-tight text-center text-black">
+            <a>회원가입</a> / <a>로그인</a>
+          </span>
+        </div>
       </main>
-
-      {/* Footer 영역 */}
+      <Footer />
     </div>
   );
 }
